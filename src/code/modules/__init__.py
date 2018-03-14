1e930488cc5faa0180b67ba63298ae0de2b09149
dengl11 <dengl11@stanford.edu>
2018-03-13 21:07:19 -0700

    add double lstm output

--- src/code/modules/__init__.py
+++ src/code/modules/__init__.py
@@ -4,6 +4,8 @@ from attn_layer import *
 from pred_layer import * 
 from output_layer import * 
 from output_lstm import * 
+from output_double_lstm import *
+from output_double_lstm_dense import *
 # from .attn_basic import *
 # from .attn_bidirection import *
 # from .attn_co import *
--- src/code/modules/output_layer.py
+++ src/code/modules/output_layer.py
@@ -3,6 +3,8 @@
 import sys 
 from output_basic import OutputBasic
 from output_lstm import *
+from output_double_lstm import *
+from output_double_lstm_dense import *
 
 def get_output_layer(name, output_sz, keep_prob):
     """get a output layer class
@@ -15,5 +17,9 @@ def get_output_layer(name, output_sz, keep_prob):
         return OutputBasic(output_sz, keep_prob)
     elif name == "lstm":
         return OutputLSTM(output_sz, keep_prob)
+    elif name == "double_lstm":
+        return OutputDouhleLSTM(output_sz, keep_prob)
+    elif name == "double_lstm_dense":
+        return OutputDoubleLSTMDense(output_sz, keep_prob)
     else:
         sys.exit("No such Output Layer!")
