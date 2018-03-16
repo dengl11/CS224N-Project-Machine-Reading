2412d795d0901268524ff7a41b17b8998ec1d0cf
zhiling <zhilinghuang2013@gmail.com>
2018-03-15 18:36:07 -0700

    First draft of combined attention.

--- src/code/modules/attn_layer.py
+++ src/code/modules/attn_layer.py
@@ -5,6 +5,7 @@ from .attn_basic import *
 from .attn_bidirection import *
 from .attn_co import *
 from .attn_self import *
+from .attn_combined import  *
 
 def get_attn_layer(name, keep_prob, context_size, question_size):
     """get an attention layer class
@@ -30,6 +31,10 @@ def get_attn_layer(name, keep_prob, context_size, question_size):
         attn_layer = SelfAttn(keep_prob,
                               context_size,
                               question_size)
+    elif name == 'combined_attn':
+        attn_layer = CombinedAttn(keep_prob,
+                                  context_size,
+                                  question_size)
 
     else:
         sys.exit("no such Attention Layer: {}!".format(name))
