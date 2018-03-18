b5e26eb02adf7e104e719aa73329d93b81b36c9e
zhiling <zhilinghuang2013@gmail.com>
2018-03-18 14:13:38 -0700

    Change pred layer to be max p_start(i) * p_start(j).
    Also created new make template.

--- src/code/modules/pred_dense_softmax.py
+++ src/code/modules/pred_dense_softmax.py
@@ -10,7 +10,7 @@ class PredictionDenseSoftmax(PredictionBasic):
     def __init__(self, hidden_sz):
         self.scope = "pred_dense_softmax"
         self.hidden_sz = hidden_sz
-        self.answer_length = 10
+        self.answer_length = 15
   
     def build_graph(self, reps, context_mask):
         """give the final prediction for start_pos and end_pos
@@ -29,22 +29,24 @@ class PredictionDenseSoftmax(PredictionBasic):
                                                      num_outputs=self.hidden_sz) 
             logits_start, probdist_start = self._pred_start(reps, context_mask)
             logits_end,   probdist_end   = self._pred_end(reps, context_mask)
-            # The value at each index is prob_end(index) + prob_end(index+1) ....
-            end_prob_sum = tf.identity(probdist_end)
-            # The value at each index is prob_start(index) + prob_start(index-1) ...
-            start_prob_sum = tf.identity(probdist_start)
+
+            # The value at each index is max(prob_end(index), prob_end(index+1) ....)
+            end_prob_max = tf.identity(probdist_end)
+            # The value at each index is max(prob_start(index), prob_start(index-1) ...)
+            start_prob_max = tf.identity(probdist_start)
 
             for index in range(1, self.answer_length):
                 padding_at_end = tf.constant([[0, 0], [0, index]])
                 padding_at_start = tf.constant([[0, 0], [index, 0]])
-                end_prob_sum = tf.add(end_prob_sum,
+
+                end_prob_max = tf.maximum(end_prob_max,
                                       tf.pad(probdist_end[:, index:],
                                              padding_at_end, "CONSTANT"))
-                start_prob_sum = tf.add(end_prob_sum,
+                start_prob_max = tf.maximum(end_prob_max,
                                         tf.pad(probdist_start[:, :-index],
                                                padding_at_start, "CONSTANT"))
-            probdist_start = probdist_start * end_prob_sum
-            probdist_end   = probdist_end * start_prob_sum
+            probdist_start = probdist_start * end_prob_max
+            probdist_end   = probdist_end * start_prob_max
 
             # probdist_start = tf.nn.l2_normalize(probdist_start, 1)
             # probdist_end = tf.nn.l2_normalize(probdist_end, 1)
