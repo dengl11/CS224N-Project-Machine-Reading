3202c4d0190756be16c1cf1a4afda16c53475449
dengl11 <dengl11@stanford.edu>
2018-03-20 21:37:36 +0000

    debug eval

--- src/code/main.py
+++ src/code/main.py
@@ -237,17 +237,18 @@ def main(unused_argv):
             with tf.Session(config=config) as sess:
 
                 # Load best model
-                qa_model.initialize_from_checkpoint(sess, bestmodel_dir, expect_exists=True)
+                qa_model.initialize_from_checkpoint(sess, FLAGS.ckpt_load_dir, expect_exists=True)
 
+                logger.info("Model initialzed from checkpoint")
                 # train
                 train_f1, train_em = qa_model.check_f1_em(sess, train_context_path,
                                             train_qn_path, train_ans_path,
-                                            "train", num_samples=100000000000,
+                                            "train", num_samples=100,
                                             print_to_screen=False)
                 # dev
                 dev_f1, dev_em = qa_model.check_f1_em(sess, dev_context_path,
                                             dev_qn_path, dev_ans_path,
-                                            "dev", num_samples=10000000000,
+                                            "dev", num_samples=100,
                                             print_to_screen=False)
         logger.error("Train: F1 = {:.3}, EM = {:.3}".format(train_f1, train_em))
         logger.error("Dev:   F1 = {:.3}, EM = {:.3}".format(dev_f1, dev_em))
